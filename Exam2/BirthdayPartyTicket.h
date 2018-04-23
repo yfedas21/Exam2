62a2cd86f7cd3c19ada9f630714033291c611be4
Yuriy Fedas <yfedas21@my.whitworth.edu>
2018-04-23 15:29:40 -0700

    Added commenting as a final touch.

--- Exam2/BirthdayPartyTicket.h
+++ Exam2/BirthdayPartyTicket.h
@@ -9,13 +9,23 @@
 class BirthdayPartyTicket : public PartyTicket
 {
 private: 
-	Party * the_party;
-	std::list<std::string>::iterator me;
+
+	Party * the_party; // The party this ticket is associated with
+	std::list<std::string>::iterator me; // My location in the BirthdayParty list
 
 public:
+	/**
+	* Constructor for creating a BirthdayPartyTicket.
+	* @param the_party party to which this ticket belongs
+	* @param me		   party goer to which this ticket is associated with
+	*/
 	BirthdayPartyTicket(Party *the_party, std::list<std::string>::iterator &me) :
 		the_party(the_party), me(me) {}
 
+	/**
+	* Allows the partygoer (me) to leave the party "the_party"
+	* The implementation for this is in BirthdayPartyTicket.cpp.
+	*/
 	void leave();
 };
 
